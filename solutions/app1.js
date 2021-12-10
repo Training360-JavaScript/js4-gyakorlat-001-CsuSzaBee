84ea18ba6a0329815f840b7851d41fa844b522a1
Szabolcs Csurgó <csurgo.szabolcs@gmail.com>
2021-12-10 12:46:00 +0100

    js4- 001 solutions

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+function converterPromise(array) {
+  return new Promise((resolve, reject) => {
+    if (array.find(el => typeof el !== "string") === undefined) {
+      resolve(array.map(item => item.toUpperCase()))
+    } else {
+      reject('Error: Not all elements are string type!')
+    }
+  })
+}
 
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
